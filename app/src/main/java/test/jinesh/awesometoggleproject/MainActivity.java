1b3134d13dbba1195720882eb44112f5095a5de7
jineshfrancs <jineshfofficial@gmail.com>
2016-11-22 14:09:30 +0530

    Changes

--- app/src/main/java/test/jinesh/awesometoggleproject/MainActivity.java
+++ app/src/main/java/test/jinesh/awesometoggleproject/MainActivity.java
@@ -4,10 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    AwesomeToggle awesomeToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        awesomeToggle= (AwesomeToggle) findViewById(R.id.sample);
+        awesomeToggle.setOnCheckedChangeListner(new AwesomeToggle.OnCheckedChangeListner() {
+            @Override
+            public void onChecked(boolean isChecked) {
+
+            }
+        });
     }
 }
